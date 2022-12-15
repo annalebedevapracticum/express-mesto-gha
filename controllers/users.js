23ea2b1313ae25c5ca8c803ef47683a9eae4f9cb
Anna Lebedeva <anna.lebedeva.practicum@yandex.ru>
2022-12-15 20:12:21 +0300

    cookie

--- controllers/users.js
+++ controllers/users.js
@@ -101,8 +101,6 @@ module.exports.login = (req, res, next) => {
       res
         .cookie('jwt', token, {
           maxAge: 3600000,
-          httpOnly: true,
-          sameSite: true,
         }).send({ token });
     })
     .catch((err) => {
