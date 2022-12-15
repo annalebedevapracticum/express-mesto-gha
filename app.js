513f10bc820122313671e4f5d6bc411fc50e66dd
Anna Lebedeva <anna.lebedeva.practicum@yandex.ru>
2022-12-15 18:34:26 +0300

    fix

--- app.js
+++ app.js
@@ -36,6 +36,8 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
   }).unknown(true),
 }), createUser);
 
--- controllers/users.js
+++ controllers/users.js
@@ -42,6 +42,8 @@ module.exports.createUser = (req, res, next) => {
         res.status(201).send({
           _id: user._id,
           email: user.email,
+          name,
+          about,
         });
       })
       .catch((err) => {
