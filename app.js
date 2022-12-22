2562c3ddcd377e98c79af9089a6cfeab33d69882
Anna Lebedeva <anna.lebedeva.practicum@yandex.ru>
2022-12-22 14:48:08 +0300

    rework routes

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const { errors, celebrate, Joi } = require('celebrate');
+const { errors } = require('celebrate');
 const process = require('process');
-const { login, createUser } = require('./controllers/users');
-const { checkAuth } = require('./middlewares/auth');
-const { urlRegex } = require('./helpers/utils');
-const CustomError = require('./helpers/CustomError');
+const routes = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -28,31 +25,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   console.log('Connected to MongoDB!!!');
 });
 
-app.use('/cards', checkAuth, require('./routes/cards'));
-app.use('/users', checkAuth, require('./routes/users'));
-
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(urlRegex),
-  }),
-}), createUser);
-
-app.use('*', checkAuth, (req, res, next) => next(new CustomError('url not found', 404)));
 app.listen(PORT, () => {
   console.log(`Server is working! Port: ${PORT}`);
 });
-
 app.use(errors());
+app.use(routes);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
---
+++ routes/index.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const { Joi, celebrate } = require('celebrate');
+const { login, createUser } = require('../controllers/users');
+const CustomError = require('../helpers/CustomError');
+const { urlRegex } = require('../helpers/utils');
+const { checkAuth } = require('../middlewares/auth');
+
+router.use('/cards', checkAuth, require('./cards'));
+router.use('/users', checkAuth, require('./users'));
+
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(urlRegex),
+  }),
+}), createUser);
+
+router.use('*', checkAuth, (req, res, next) => next(new CustomError('url not found', 404)));
+
+module.exports = router;
