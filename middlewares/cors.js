85c36ea27d64b0b80affa636e216491c0f264958
Anna Lebedeva <anna.lebedeva.practicum@yandex.ru>
2023-01-04 19:27:42 +0300

    add test library; add cors middlewar

---
+++ middlewares/cors.js
@@ -0,0 +1,28 @@
+// Массив доменов, с которых разрешены кросс-доменные запросы
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+];
+
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+const corsMiddlewar = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  return next();
+};
+
+module.exports = {
+  corsMiddlewar,
+};
